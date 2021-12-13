be49fa00208e0adc4c6d454385ed5b4cb43926f6
j0le <55405106+xj0le@users.noreply.github.com>
2021-12-13 22:48:27 +0100

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,1 +1,13 @@
+-- Syncanje sati u igri sa realnim vremenom
 
+RegisterNetEvent("j0le:client:SyncajSate")
+AddEventHandler("j0le:client:SyncajSate", function(Sati, Minute, Sekunde)
+    NetworkOverrideClockTime(Sati, Minute, Sekunde)
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        TriggerServerEvent("j0le:server:SyncajSate")
+        Citizen.Wait(Config.MinuteSycanjaSati * 60000)
+    end
+end)
