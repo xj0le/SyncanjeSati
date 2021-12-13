ccd4fadc0b84efe75b20905250970d5c7b962842
j0le <55405106+xj0le@users.noreply.github.com>
2021-12-13 22:48:42 +0100

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,1 +1,6 @@
+-- Syncanje sati u igri sa realnim vremenom
 
+RegisterNetEvent("j0le:server:SyncajSate")
+AddEventHandler("j0le:server:SyncajSate", function()
+    TriggerClientEvent("j0le:client:SyncajSate", source, tonumber(os.date("%H")), tonumber(os.date("%M")), tonumber(os.date("%S")))
+end)
